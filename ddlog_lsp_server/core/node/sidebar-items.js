83ea0541203d1f81a182bd1376cafe9f048b16a9
silvanshade <silvanshade@users.noreply.github.com>
2021-04-24 02:17:22 +0000

    deploy: d1319388412e10200042a3201d760577f0770d49

--- ddlog_lsp_server/core/node/sidebar-items.js
+++ ddlog_lsp_server/core/node/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["NodeWalker","The current state of the node walking and token encoding algorithm."],["NodeWalkerContext","The current node context."],["NodeWalkerLevel",""]]});
+initSidebarItems({"mod":[["context",""]],"struct":[["NodeWalker",""]],"trait":[["Context",""]],"type":[["BasicNodeWalker",""],["TraceNodeWalker",""]]});
